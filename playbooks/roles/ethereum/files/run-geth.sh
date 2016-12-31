656516fd2a342e265bf93c3bc9afbedb17998fa6
George Pîrlea <george@dranov.ro>
2016-12-31 18:10:26 +0000

    Allow personal API over JSONRPC; parity CORS

--- playbooks/roles/ethereum/files/run-geth.sh
+++ playbooks/roles/ethereum/files/run-geth.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-geth --rpc --rpcapi "web3,eth" --rpccorsdomain "http://localhost:8000" --testnet --fast --cache=1024 --jitvm console
+geth --rpc --rpcapi "web3,eth,personal" --rpccorsdomain "http://localhost:8000" --testnet --fast --cache=1024 --jitvm console
--- playbooks/roles/parity/files/run-parity.sh
+++ playbooks/roles/parity/files/run-parity.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-parity ui --testnet --warp
+parity ui --testnet --warp --rpc --rpcapi "web3,eth,personal"  --rpccorsdomain "http://localhost:8000"
